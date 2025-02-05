03669673b3b60a0ac5d61ebcc6b8e351b73c1632
user82304z7x <9716194+user82304z7x@users.noreply.github.com>
2025-02-05 11:47:34 +0000

    [Docs Site] Rename /changelog-next/ to /changelog/, /changelog/ to /release-notes/ (#19675)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -63,6 +63,28 @@ describe("Cloudflare Docs", () => {
 		expect(response.headers.get("Location")).toBe("/products/");
 	});
 
+	it("redirects /changelog/index.xml to /release-notes/index.xml", async () => {
+		const request = new Request("http://fakehost/changelog/index.xml");
+		const response = await SELF.fetch(request, { redirect: "manual" });
+		expect(response.status).toBe(301);
+		expect(response.headers.get("Location")).toBe("/release-notes/index.xml");
+	});
+
+	it("redirects /changelog-next/ to /changelog/", async () => {
+		const request = new Request("http://fakehost/changelog-next/");
+		const response = await SELF.fetch(request, { redirect: "manual" });
+		expect(response.status).toBe(301);
+		expect(response.headers.get("Location")).toBe("/changelog/");
+	});
+
+	it("redirects /changelog-next/rss.xml to /changelog/rss.xml", async () => {
+		const request = new Request("http://fakehost/changelog-next/rss.xml");
+		const response = await SELF.fetch(request, { redirect: "manual" });
+
+		expect(response.status).toBe(301);
+		expect(response.headers.get("Location")).toBe("/changelog/rss.xml");
+	});
+
 	it("responds with 404.html at `/non-existent`", async () => {
 		const request = new Request("http://fakehost/non-existent");
 		const response = await SELF.fetch(request);
