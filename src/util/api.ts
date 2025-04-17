3c2a14ea95cd717853855a2c168fdeea5e8087a4
user82304z7x <9716194+user82304z7x@users.noreply.github.com>
2025-04-17 15:52:29 +0000

    [Docs Site] Update pinned api-schemas commit (#21775)
    * [Docs Site] Update pinned api-schemas commit to 9b9382ef73e94ccf9d833175658cf2fc891f13e5
    
    * Update src/util/api.ts
    
    * Update src/util/api.ts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Kody Jackson <kody@cloudflare.com>

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
-const COMMIT = "d45020bb7aea758ee1997a892f508f26592d304f";
+const COMMIT = "9b9382ef73e94ccf9d833175658cf2fc891f13e5";
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
